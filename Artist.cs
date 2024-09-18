f289787ed3dee956efa1d23e713a575c2069fec6
Shua'a <51505005+Shuaa-99@users.noreply.github.com>
2024-09-18 16:39:57 +0300

    Artist

---
+++ Artist.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_commerce_
+{
+    public class Artist
+    {
+        public int id { set; get; }
+        public string Name { set; get; }
+        public string description { set; get; }
+        public string Email { set; get; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+
+
+    }
+}
